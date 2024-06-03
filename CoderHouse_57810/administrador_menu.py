60bbb9c43b320724077047272976e78ada0bf255
Ginette Laglere <glaglere@gmail.com>
2024-06-02 21:21:18 -0300

    se mejora la mantenibilidad agregando funciones que son llamadas desde los menues, evitando metodos largos

--- CoderHouse_57810/administrador_menu.py
+++ CoderHouse_57810/administrador_menu.py
@@ -3,20 +3,202 @@ from tabulate import tabulate
 from CoderHouse_57810.models.administrador import Administrador
 from CoderHouse_57810.models.cliente import ClientePersona, ClienteCorporativo
 from CoderHouse_57810.models.producto import Producto
-from CoderHouse_57810.services.helpers import print_menu, get_option, collect_input, validations
+from CoderHouse_57810.services.helpers import print_menu, get_option, collect_input
+
+MENU_OPTIONS = [
+    "Agregar Cliente Persona", "Agregar Cliente Corporativo", "Agregar Administrador",
+    "Agregar Producto", "Mostrar Clientes", "Mostrar Administradores", "Mostrar Productos",
+    "Eliminar Cliente Persona", "Eliminar Cliente Corporativo", "Eliminar Administrador",
+    "Eliminar Producto", "Ver Historial de Compras de Todos los Clientes",
+    "Regresar al menú principal"
+]
+
 
 def mostrar_menu_administradores():
     """
     Muestra el menú principal de administradores.
     """
-    options = [
-        "Agregar Cliente Persona", "Agregar Cliente Corporativo", "Agregar Administrador",
-        "Agregar Producto", "Mostrar Clientes", "Mostrar Administradores", "Mostrar Productos",
-        "Eliminar Cliente Persona", "Eliminar Cliente Corporativo", "Eliminar Administrador",
-        "Eliminar Producto", "Ver Historial de Compras de Todos los Clientes",
-        "Regresar al menú principal"
-    ]
-    print_menu(options)
+    print_menu(MENU_OPTIONS)
+
+
+def agregar_cliente_persona(sistema):
+    """
+    Agrega un cliente persona al sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    data = collect_input(["nombre", "email", "contraseña", "dirección", "teléfono", "DNI"])
+    cliente = ClientePersona(data["nombre"], data["email"], data["contraseña"], data["dirección"],
+                             data["teléfono"], data["DNI"])
+    if sistema.agregar_cliente_persona(cliente):
+        print("Cliente Persona agregado exitosamente.")
+    else:
+        print("Error al agregar Cliente Persona.")
+
+
+def agregar_cliente_corporativo(sistema):
+    """
+    Agrega un cliente corporativo al sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    data = collect_input(["nombre", "email", "contraseña", "dirección", "teléfono", "CUIT"])
+    cliente = ClienteCorporativo(data["nombre"], data["email"], data["contraseña"], data["dirección"],
+                                 data["teléfono"], data["CUIT"])
+    if sistema.agregar_cliente_corporativo(cliente):
+        print("Cliente Corporativo agregado exitosamente.")
+    else:
+        print("Error al agregar Cliente Corporativo.")
+
+
+def agregar_administrador(sistema):
+    """
+    Agrega un administrador al sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    data = collect_input(["nombre", "email", "contraseña", "código funcionario"])
+    admin = Administrador(data["nombre"], data["email"], data["contraseña"], data["código funcionario"])
+    if sistema.agregar_administrador(admin):
+        print("Administrador agregado exitosamente.")
+    else:
+        print("Error al agregar Administrador.")
+
+
+def agregar_producto(sistema):
+    """
+    Agrega un producto al sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    try:
+        data = collect_input(["nombre del producto", "descripción del producto", "categoría del producto",
+                              "precio del producto"])
+        producto = Producto(data["nombre del producto"], data["descripción del producto"],
+                            data["categoría del producto"], float(data["precio del producto"]))
+        if sistema.agregar_producto(producto):
+            print("Producto agregado exitosamente.")
+        else:
+            print("Error al agregar Producto.")
+    except ValueError as e:
+        print(f"Error al agregar Producto: {e}")
+
+
+def mostrar_clientes(sistema):
+    """
+    Muestra todos los clientes del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    sistema.mostrar_clientes()
+
+
+def mostrar_administradores(sistema):
+    """
+    Muestra todos los administradores del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    sistema.mostrar_administradores()
+
+
+def mostrar_productos(sistema):
+    """
+    Muestra todos los productos del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    sistema.mostrar_productos()
+
+
+def eliminar_cliente_persona(sistema):
+    """
+    Elimina un cliente persona del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    email = input("Ingrese el email del cliente persona a eliminar: ")
+    if sistema.eliminar_cliente_persona(email):
+        print("Cliente Persona eliminado exitosamente.")
+    else:
+        print("Error al eliminar Cliente Persona.")
+
+
+def eliminar_cliente_corporativo(sistema):
+    """
+    Elimina un cliente corporativo del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    email = input("Ingrese el email del cliente corporativo a eliminar: ")
+    if sistema.eliminar_cliente_corporativo(email):
+        print("Cliente Corporativo eliminado exitosamente.")
+    else:
+        print("Error al eliminar Cliente Corporativo.")
+
+
+def eliminar_administrador(sistema):
+    """
+    Elimina un administrador del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    email = input("Ingrese el email del administrador a eliminar: ")
+    if sistema.eliminar_administrador(email):
+        print("Administrador eliminado exitosamente.")
+    else:
+        print("Error al eliminar Administrador.")
+
+
+def eliminar_producto(sistema):
+    """
+    Elimina un producto del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    if not sistema.productos:
+        print("No hay productos disponibles para eliminar.")
+        return
+    id_producto = input("Ingrese el ID del producto a eliminar: ")
+    if sistema.eliminar_producto(id_producto):
+        print("Producto eliminado exitosamente.")
+    else:
+        print("Error al eliminar Producto.")
+
+
+def ver_historial_compras_todos(sistema):
+    """
+    Muestra el historial de compras de todos los clientes del sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    compras = sistema.obtener_historial_compras_todos()
+    if compras:
+        print("Historial de Compras de Todos los Clientes:")
+        table = []
+        for compra in compras:
+            productos_str = ", ".join(
+                [f"{prod['producto'].nombre} x{prod['cantidad']}" for prod in compra.productos])
+            precio_total = sum([prod['producto'].precio * prod['cantidad'] for prod in compra.productos])
+            table.append([compra.cliente.nombre, compra.cliente.email, productos_str, precio_total,
+                          compra.fecha.strftime('%Y-%m-%d %H:%M:%S')])
+        print(tabulate(table, headers=["Cliente", "Email", "Productos", "Precio Total", "Fecha de Compra"],
+                       tablefmt="pretty"))
+    else:
+        print("No hay compras registradas.")
+
 
 def operaciones_administradores(sistema):
     """
@@ -27,95 +209,30 @@ def operaciones_administradores(sistema):
     """
     while True:
         mostrar_menu_administradores()
-        opcion = get_option([
-            "Agregar Cliente Persona", "Agregar Cliente Corporativo", "Agregar Administrador",
-            "Agregar Producto", "Mostrar Clientes", "Mostrar Administradores", "Mostrar Productos",
-            "Eliminar Cliente Persona", "Eliminar Cliente Corporativo", "Eliminar Administrador",
-            "Eliminar Producto", "Ver Historial de Compras de Todos los Clientes",
-            "Regresar al menú principal"
-        ])
+        opcion = get_option(MENU_OPTIONS)
         if opcion == 1:
-            data = collect_input(["nombre", "email", "contraseña", "dirección", "teléfono", "DNI"])
-            cliente = ClientePersona(data["nombre"], data["email"], data["contraseña"], data["dirección"],
-                                     data["teléfono"], data["DNI"])
-            if sistema.agregar_cliente_persona(cliente):
-                print("Cliente Persona agregado exitosamente.")
-            else:
-                print("Error al agregar Cliente Persona.")
+            agregar_cliente_persona(sistema)
         elif opcion == 2:
-            data = collect_input(["nombre", "email", "contraseña", "dirección", "teléfono", "CUIT"])
-            cliente = ClienteCorporativo(data["nombre"], data["email"], data["contraseña"], data["dirección"],
-                                         data["teléfono"], data["CUIT"])
-            if sistema.agregar_cliente_corporativo(cliente):
-                print("Cliente Corporativo agregado exitosamente.")
-            else:
-                print("Error al agregar Cliente Corporativo.")
+            agregar_cliente_corporativo(sistema)
         elif opcion == 3:
-            data = collect_input(["nombre", "email", "contraseña", "código funcionario"])
-            admin = Administrador(data["nombre"], data["email"], data["contraseña"], data["código funcionario"])
-            if sistema.agregar_administrador(admin):
-                print("Administrador agregado exitosamente.")
-            else:
-                print("Error al agregar Administrador.")
+            agregar_administrador(sistema)
         elif opcion == 4:
-            try:
-                data = collect_input(["nombre del producto", "descripción del producto", "categoría del producto",
-                                      "precio del producto"])
-                producto = Producto(data["nombre del producto"], data["descripción del producto"],
-                                    data["categoría del producto"], float(data["precio del producto"]))
-                if sistema.agregar_producto(producto):
-                    print("Producto agregado exitosamente.")
-                else:
-                    print("Error al agregar Producto.")
-            except ValueError as e:
-                print(f"Error al agregar Producto: {e}")
+            agregar_producto(sistema)
         elif opcion == 5:
-            sistema.mostrar_clientes()
+            mostrar_clientes(sistema)
         elif opcion == 6:
-            sistema.mostrar_administradores()
+            mostrar_administradores(sistema)
         elif opcion == 7:
-            sistema.mostrar_productos()
+            mostrar_productos(sistema)
         elif opcion == 8:
-            email = input("Ingrese el email del cliente persona a eliminar: ")
-            if sistema.eliminar_cliente_persona(email):
-                print("Cliente Persona eliminado exitosamente.")
-            else:
-                print("Error al eliminar Cliente Persona.")
+            eliminar_cliente_persona(sistema)
         elif opcion == 9:
-            email = input("Ingrese el email del cliente corporativo a eliminar: ")
-            if sistema.eliminar_cliente_corporativo(email):
-                print("Cliente Corporativo eliminado exitosamente.")
-            else:
-                print("Error al eliminar Cliente Corporativo.")
+            eliminar_cliente_corporativo(sistema)
         elif opcion == 10:
-            email = input("Ingrese el email del administrador a eliminar: ")
-            if sistema.eliminar_administrador(email):
-                print("Administrador eliminado exitosamente.")
-            else:
-                print("Error al eliminar Administrador.")
+            eliminar_administrador(sistema)
         elif opcion == 11:
-            if not sistema.productos:
-                print("No hay productos disponibles para eliminar.")
-                continue
-            id_producto = input("Ingrese el ID del producto a eliminar: ")
-            if sistema.eliminar_producto(id_producto):
-                print("Producto eliminado exitosamente.")
-            else:
-                print("Error al eliminar Producto.")
+            eliminar_producto(sistema)
         elif opcion == 12:
-            compras = sistema.obtener_historial_compras_todos()
-            if compras:
-                print("Historial de Compras de Todos los Clientes:")
-                table = []
-                for compra in compras:
-                    productos_str = ", ".join(
-                        [f"{prod['producto'].nombre} x{prod['cantidad']}" for prod in compra.productos])
-                    precio_total = sum([prod['producto'].precio * prod['cantidad'] for prod in compra.productos])
-                    table.append([compra.cliente.nombre, compra.cliente.email, productos_str, precio_total,
-                                  compra.fecha.strftime('%Y-%m-%d %H:%M:%S')])
-                print(tabulate(table, headers=["Cliente", "Email", "Productos", "Precio Total", "Fecha de Compra"],
-                               tablefmt="pretty"))
-            else:
-                print("No hay compras registradas.")
+            ver_historial_compras_todos(sistema)
         elif opcion == 13:
             break
--- CoderHouse_57810/cliente_menu.py
+++ CoderHouse_57810/cliente_menu.py
@@ -4,7 +4,11 @@ from tabulate import tabulate
 
 from CoderHouse_57810.services.helpers import print_menu, get_option, collect_input
 
-
+MENU_OPTIONS_CLIENTE_LOGUEADO = [
+        "Agregar Producto al Carrito", "Mostrar Carrito", "Quitar Producto del Carrito",
+        "Concretar Compra", "Ver Historial de Compras", "Listar Todos los Productos",
+        "Listar Productos por Categoría", "Salir"
+    ]
 def mostrar_menu_clientes():
     """
     Muestra el menú principal de clientes.
@@ -17,12 +21,7 @@ def mostrar_menu_cliente_logueado():
     """
     Muestra el menú de opciones para clientes logueados.
     """
-    options = [
-        "Agregar Producto al Carrito", "Mostrar Carrito", "Quitar Producto del Carrito",
-        "Concretar Compra", "Ver Historial de Compras", "Listar Todos los Productos",
-        "Listar Productos por Categoría", "Salir"
-    ]
-    print_menu(options)
+    print_menu(MENU_OPTIONS_CLIENTE_LOGUEADO)
 
 
 def login(email, password, usuarios):
@@ -55,7 +54,8 @@ def operaciones_clientes(sistema):
         opcion = get_option(["Loguearse", "Regresar al menú principal"])
         if opcion == 1:
             credentials = collect_input(["email", "contraseña"])
-            cliente = login(credentials["email"], credentials["contraseña"], sistema.clientes_personas + sistema.clientes_corporativos)
+            cliente = login(credentials["email"], credentials["contraseña"],
+                            sistema.clientes_personas + sistema.clientes_corporativos)
             if cliente:
                 print(f"Bienvenido {cliente.nombre}")
                 operaciones_cliente_logueado(sistema, cliente)
@@ -63,6 +63,134 @@ def operaciones_clientes(sistema):
                 print("Credenciales incorrectas.")
         elif opcion == 2:
             break
+
+
+def agregar_producto_al_carrito(sistema, cliente):
+    """
+    Permite al cliente agregar un producto al carrito.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+        cliente (ClientePersona o ClienteCorporativo): El cliente logueado.
+    """
+    if not sistema.productos:
+        print("No hay productos disponibles.")
+        return
+    sistema.mostrar_productos()
+    try:
+        id_producto = int(input("Ingrese el ID del producto a agregar al carrito: "))
+        cantidad = int(input("Ingrese la cantidad del producto a agregar al carrito: "))
+    except ValueError:
+        print("ID o cantidad de producto no válidos.")
+        return
+    producto = next((p for p in sistema.productos if p.id_producto == id_producto), None)
+    if producto:
+        sistema.agregar_producto_al_carrito(cliente.email, producto, cantidad)
+        print("Producto agregado al carrito.")
+    else:
+        print("Producto no encontrado.")
+
+
+def mostrar_carrito(sistema, cliente):
+    """
+    Muestra el contenido del carrito del cliente.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+        cliente (ClientePersona o ClienteCorporativo): El cliente logueado.
+    """
+    carrito = sistema.mostrar_carrito(cliente.email)
+    if carrito:
+        print("Carrito:")
+        table = [[item["producto"].id_producto, item["producto"].nombre, item["producto"].descripcion,
+                  item["producto"].categoria, item["producto"].precio, item["cantidad"]] for item in carrito]
+        print(tabulate(table, headers=["ID", "Nombre", "Descripción", "Categoría", "Precio", "Cantidad"],
+                       tablefmt="pretty"))
+    else:
+        print("El carrito está vacío.")
+
+
+def quitar_producto_del_carrito(sistema, cliente):
+    """
+    Permite al cliente quitar un producto del carrito.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+        cliente (ClientePersona o ClienteCorporativo): El cliente logueado.
+    """
+    try:
+        id_producto = int(input("Ingrese el ID del producto a quitar del carrito: "))
+        sistema.quitar_producto_del_carrito(cliente.email, id_producto)
+        print("Producto quitado del carrito.")
+    except ValueError as e:
+        print(e)
+
+
+def concretar_compra(sistema, cliente):
+    """
+    Permite al cliente concretar la compra de los productos en el carrito.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+        cliente (ClientePersona o ClienteCorporativo): El cliente logueado.
+    """
+    try:
+        compra = sistema.concretar_compra(cliente.email)
+        if compra:
+            print("Compra concretada exitosamente.")
+    except ValueError as e:
+        print(e)
+
+
+def ver_historial_de_compras(sistema, cliente):
+    """
+    Muestra el historial de compras del cliente.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+        cliente (ClientePersona o ClienteCorporativo): El cliente logueado.
+    """
+    historial_compras = sistema.obtener_historial_compras_cliente(cliente.email)
+    if historial_compras:
+        print("Historial de Compras:")
+        for compra in historial_compras:
+            print(compra)
+    else:
+        print("No hay compras registradas.")
+
+
+def listar_todos_los_productos(sistema):
+    """
+    Lista todos los productos disponibles en el sistema.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    sistema.mostrar_productos()
+
+
+def listar_productos_por_categoria(sistema):
+    """
+    Lista los productos disponibles en el sistema por categoría.
+
+    Args:
+        sistema (Sistema): La instancia del sistema.
+    """
+    categorias = sistema.obtener_categorias_productos()
+    if not categorias:
+        print("No hay categorías disponibles.")
+        return
+    print("Categorías:")
+    table = [[index + 1, categoria] for index, categoria in enumerate(categorias)]
+    print(tabulate(table, headers=["Opción", "Categoría"], tablefmt="pretty"))
+    try:
+        opcion_categoria = int(input("Seleccione una categoría: ")) - 1
+        if 0 <= opcion_categoria < len(categorias):
+            sistema.mostrar_productos_por_categoria(categorias[opcion_categoria])
+        else:
+            print("Opción no válida.")
+    except ValueError:
+        print("Entrada no válida.")
 
 
 def operaciones_cliente_logueado(sistema, cliente):
@@ -75,76 +203,20 @@ def operaciones_cliente_logueado(sistema, cliente):
     """
     while True:
         mostrar_menu_cliente_logueado()
-        opcion = get_option([
-            "Agregar Producto al Carrito", "Mostrar Carrito", "Quitar Producto del Carrito",
-            "Concretar Compra", "Ver Historial de Compras", "Listar Todos los Productos",
-            "Listar Productos por Categoría", "Salir"
-        ])
+        opcion = get_option(MENU_OPTIONS_CLIENTE_LOGUEADO)
         if opcion == 1:
-            if not sistema.productos:
-                print("No hay productos disponibles.")
-                continue
-            sistema.mostrar_productos()
-            try:
-                id_producto = int(input("Ingrese el ID del producto a agregar al carrito: "))
-                cantidad = int(input("Ingrese la cantidad del producto a agregar al carrito: "))
-            except ValueError:
-                print("ID o cantidad de producto no válidos.")
-                continue
-            producto = next((p for p in sistema.productos if p.id_producto == id_producto), None)
-            if producto:
-                sistema.agregar_producto_al_carrito(cliente.email, producto, cantidad)
-                print("Producto agregado al carrito.")
-            else:
-                print("Producto no encontrado.")
+            agregar_producto_al_carrito(sistema, cliente)
         elif opcion == 2:
-            carrito = sistema.mostrar_carrito(cliente.email)
-            if carrito:
-                print("Carrito:")
-                table = [[item["producto"].id_producto, item["producto"].nombre, item["producto"].descripcion,
-                          item["producto"].categoria, item["producto"].precio, item["cantidad"]] for item in carrito]
-                print(tabulate(table, headers=["ID", "Nombre", "Descripción", "Categoría", "Precio", "Cantidad"], tablefmt="pretty"))
-            else:
-                print("El carrito está vacío.")
+            mostrar_carrito(sistema, cliente)
         elif opcion == 3:
-            try:
-                id_producto = int(input("Ingrese el ID del producto a quitar del carrito: "))
-                sistema.quitar_producto_del_carrito(cliente.email, id_producto)
-                print("Producto quitado del carrito.")
-            except ValueError as e:
-                print(e)
+            quitar_producto_del_carrito(sistema, cliente)
         elif opcion == 4:
-            try:
-                compra = sistema.concretar_compra(cliente.email)
-                if compra:
-                    print("Compra concretada exitosamente.")
-            except ValueError as e:
-                print(e)
+            concretar_compra(sistema, cliente)
         elif opcion == 5:
-            historial_compras = sistema.obtener_historial_compras_cliente(cliente.email)
-            if historial_compras:
-                print("Historial de Compras:")
-                for compra in historial_compras:
-                    print(compra)
-            else:
-                print("No hay compras registradas.")
+            ver_historial_de_compras(sistema, cliente)
         elif opcion == 6:
-            sistema.mostrar_productos()
+            listar_todos_los_productos(sistema)
         elif opcion == 7:
-            categorias = sistema.obtener_categorias_productos()
-            if not categorias:
-                print("No hay categorías disponibles.")
-                continue
-            print("Categorías:")
-            table = [[index + 1, categoria] for index, categoria in enumerate(categorias)]
-            print(tabulate(table, headers=["Opción", "Categoría"], tablefmt="pretty"))
-            try:
-                opcion_categoria = int(input("Seleccione una categoría: ")) - 1
-                if 0 <= opcion_categoria < len(categorias):
-                    sistema.mostrar_productos_por_categoria(categorias[opcion_categoria])
-                else:
-                    print("Opción no válida.")
-            except ValueError:
-                print("Entrada no válida.")
+            listar_productos_por_categoria(sistema)
         elif opcion == 8:
             break
