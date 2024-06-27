a3e64d8dca9ca045f7b4ca0f0456fb1ae1469344
Ginette Laglere <glaglere@gmail.com>
2024-06-27 19:56:29 -0300

    corregí bugs, apliqué, creé formularios para los models y apliqué herencia html en todos ellos

--- tercera pre-entrega_LAGLERE/sac/sac/urls.py
+++ tercera pre-entrega_LAGLERE/sac/sac/urls.py
@@ -2,10 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from webapp.views import lista_productos, inicio
+from webapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('productos/', lista_productos, name='lista_productos'),
-    path('', inicio, name='inicio'),
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('agregar_producto/', views.agregar_producto, name='agregar_producto'),
+    path('agregar_cliente/', views.agregar_cliente, name='agregar_cliente'),
+    path('agregar_empleado/', views.agregar_empleado, name='agregar_empleado'),
+    path('agregar_compra/', views.agregar_compra, name='agregar_compra'),
+    path('', views.inicio, name='inicio'),
 ]
--- tercera pre-entrega_LAGLERE/sac/webapp/views.py
+++ tercera pre-entrega_LAGLERE/sac/webapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+# webapp/views.py
+
+from django.shortcuts import render, redirect
+from .forms import ProductoForm, ClienteForm, EmpleadoForm, CompraForm
 from .models import Producto
 
 def lista_productos(request):
@@ -7,3 +10,43 @@ def lista_productos(request):
 
 def inicio(request):
     return render(request, 'webapp/inicio.html')
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'webapp/agregar_producto.html', {'form': form})
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inicio')
+    else:
+        form = ClienteForm()
+    return render(request, 'webapp/agregar_cliente.html', {'form': form})
+
+def agregar_empleado(request):
+    if request.method == 'POST':
+        form = EmpleadoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inicio')
+    else:
+        form = EmpleadoForm()
+    return render(request, 'webapp/agregar_empleado.html', {'form': form})
+
+def agregar_compra(request):
+    if request.method == 'POST':
+        form = CompraForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inicio')
+    else:
+        form = CompraForm()
+    return render(request, 'webapp/agregar_compra.html', {'form': form})
