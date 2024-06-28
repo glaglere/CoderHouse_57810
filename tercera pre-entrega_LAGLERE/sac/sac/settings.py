07cf8f60483711cdbba5e6f7538aa0c5650c5213
Ginette Laglere <glaglere@gmail.com>
2024-06-27 22:20:16 -0300

    se agregan en la pagina de inicio las principales secciones para facilitar el acceso se agregan imagenes descriptivas de las mismas

--- tercera pre-entrega_LAGLERE/sac/sac/settings.py
+++ tercera pre-entrega_LAGLERE/sac/sac/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Application definition
 
